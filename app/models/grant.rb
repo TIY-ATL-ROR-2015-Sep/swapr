9bdba3f66804dd741983a06c6e755e2a92085510
Tim Whitacre <tim@timw.co>
2015-11-19 08:43:56 -0500

    Added support for png, jpg and jpeg

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -6,7 +6,8 @@ class Grant < ActiveRecord::Base
   validates :gift, attachment_presence: true
   validates_attachment_file_name :gift, matches: [
     /mp3\Z/, /ogg\Z/, /flac\Z/, /pdf\Z/, /epub\Z/, /djvu\Z/,
-    /gif\Z/, /gifv\Z/, /mp4\Z/, /mkv\Z/, /avi\Z/, /webm\Z/
+    /gif\Z/, /gifv\Z/, /mp4\Z/, /mkv\Z/, /avi\Z/, /webm\Z/,
+    /jpg\Z/, /jpeg\Z/, /png\Z/
   ]
 
   validates_presence_of :wish_id, :user_id
